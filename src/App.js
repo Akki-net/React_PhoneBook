c615fe3a9795729e47486e8e72f84c7be99992ac
Akash Sharma <akashsharma.zak@gmail.com>
2021-09-27 21:31:14 +0530

    phonebook name showed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', id: 1}
   ]);
   const [ newName, setNewName ] = useState('');
 
@@ -14,7 +14,8 @@ const App = () => {
   const submitHandler = Event => {
     Event.preventDefault();
     const newPerson = {
-      name: newName
+      name: newName,
+      id: persons.length + 1
     };
     setPersons(persons.concat(newPerson));
     setNewName('');
@@ -32,7 +33,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(p => <p key={p.id}> {p.name} </p>)}
     </div>
   )
 }
